e6762fe1f7e52ff9dadc79edc19c90e1a247c145
Renzo Renteria <Renzo@Renzos-MacBook-Pro.local>
2017-06-06 17:16:46 -0400

    final files

---
+++ assets/javascript/game.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+
+var wins = 0;
+var losses = 0;
+var score;
+
+var minNumber = 19;
+var maxNumber = 120;
+var numberToGuess;
+
+var crystalMaxNumber = 12;
+var crystalMinNumber = 1;
+var crystalRandomNumber;
+
+function startGame(){
+	score = 0;
+
+	numberToGuess = randomNumberFromRange(minNumber, maxNumber);
+	$('#randomNumber').html(numberToGuess);
+
+	$('.crystals').each(function(index, crystalButton) {
+		$(crystalButton).data('value', randomNumberFromRange(crystalMinNumber, crystalMaxNumber));
+		$(crystalButton).on('click', onCrystalClicked);
+	});
+}
+
+function onCrystalClicked(e){
+	score += $(this).data('value');
+	$('#score').html(score);
+
+	if (score < numberToGuess){
+		return;
+	}
+
+
+	if (score === numberToGuess) {
+		wins++;
+		endGame("You won!")
+	}
+
+	else {
+		losses++;
+		endGame("You lost!")
+	}
+}
+
+function endGame(message){
+	$('#message').html(message);
+	$('#wins').html(wins);
+	$('#losses').html(losses);
+	startGame();
+}
+
+function randomNumberFromRange(min, max) {
+	return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+startGame();
+
+});
+
