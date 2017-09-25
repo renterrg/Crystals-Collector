6989738e81c7e464e20360533b925e55f2fab48d
Renzo Renteria <Renzo@Renzos-MacBook-Pro.local>
2017-09-25 12:18:15 -0400

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,61 +1,61 @@
-$(document).ready(function(){
+	$(document).ready(function(){
 
-var wins = 0;
-var losses = 0;
-var score;
+	var wins = 0;
+	var losses = 0;
+	var score;
 
-var minNumber = 19;
-var maxNumber = 120;
-var numberToGuess;
+	var minNumber = 19;
+	var maxNumber = 120;
+	var numberToGuess;
 
-var crystalMaxNumber = 12;
-var crystalMinNumber = 1;
-var crystalRandomNumber;
+	var crystalMaxNumber = 12;
+	var crystalMinNumber = 1;
+	var crystalRandomNumber;
 
-function startGame(){
-	score = 0;
+	function startGame() {
+		score = 0;
 
-	numberToGuess = randomNumberFromRange(minNumber, maxNumber);
-	$('#randomNumber').html(numberToGuess);
+		numberToGuess = randomNumberFromRange(minNumber, maxNumber);
+		$('#randomNumber').html(numberToGuess);
 
-	$('.crystals').each(function(index, crystalButton) {
-		$(crystalButton).data('value', randomNumberFromRange(crystalMinNumber, crystalMaxNumber));
-		$(crystalButton).on('click', onCrystalClicked);
-	});
-}
-
-function onCrystalClicked(e){
-	score += $(this).data('value');
-	$('#score').html(score);
-
-	if (score < numberToGuess){
-		return;
+		$('.crystals').each(function (index, crystalButton) {
+			$(crystalButton).data('value', randomNumberFromRange(crystalMinNumber, crystalMaxNumber));
+			$(crystalButton).on('click', onCrystalClicked);
+		});
 	}
 
+	function onCrystalClicked(e){
+		score += $(this).data('value');
+		$('#score').html(score);
 
-	if (score === numberToGuess) {
-		wins++;
-		endGame("You won!")
+		if (score < numberToGuess){
+			return;
+		}
+
+
+		if (score === numberToGuess) {
+			wins++;
+			endGame("You won!");
+		}
+
+		else {
+			losses++;
+			endGame("You lost!");
+		}
 	}
 
-	else {
-		losses++;
-		endGame("You lost!")
+	function endGame(message){
+		alert(message);
+		$('#wins').html(wins);
+		$('#losses').html(losses);
+		startGame();
+		$('#score').empty();
 	}
-}
 
-function endGame(message){
-	$('#message').html(message);
-	$('#wins').html(wins);
-	$('#losses').html(losses);
+	function randomNumberFromRange(min, max) {
+		return Math.floor(Math.random()*(max-min+1)+min);
+	}
+
 	startGame();
-}
 
-function randomNumberFromRange(min, max) {
-	return Math.floor(Math.random()*(max-min+1)+min);
-}
-
-startGame();
-
-});
-
+	});
